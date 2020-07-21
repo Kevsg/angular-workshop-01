f45130ec7a25dee46b1dc3efe5dc412be60cbba8
Phongpak Taothong <phongpaktao@mathregit.group>
2020-07-21 11:43:45 +0700

    Reactive Form

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
-interface Item {
-  title: string
-  done: boolean
-}
 
 @Component({
   selector: 'app-users',
@@ -11,43 +8,63 @@ interface Item {
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  loginForm: FormGroup;
+  submitted = false;
   firstName = '';
   lastName = '';
   show = true;
-  users = [
+  todos = [
     {title: "Getting Apple", done: false}
   ];
   name = '';
   done = 0;
 
-  constructor() { }
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  constructor(private formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      firstName: ['',[Validators.required, Validators.minLength(4)]],
+      lastName: ['', [Validators.required, , Validators.minLength(4)]]
+    })
   }
 
-  inputYourName(event: any): void {
-    this.firstName = event.target.value;
-  }
 
-  inputLastName(lastname: string): void {
-    this.lastName = lastname;
-  }
+  // inputYourName(event: any): void {
+  //   this.firstName = event.target.value;
+  // }
+
+  // inputLastName(lastname: string): void {
+  //   this.lastName = lastname;
+  // }
 
   toggle(index: number) {
-    if(!this.users[index].done) {
+    if(!this.todos[index].done) {
       this.done = this.done+1;
     } else {
       this.done = this.done-1;
     }
-    this.users[index].done = !this.users[index].done
+    this.todos[index].done = !this.todos[index].done
   }
 
-  save(): void {
-    if (this.firstName == null || this.firstName.trim() === ''){
-      return
+  // save(): void {
+  //   if (this.firstName == null || this.firstName.trim() === ''){
+  //     return
+  //   }
+  //   this.name = `${this.firstName} ${this.lastName}`
+  //   this.users.push({title: this.name, done: false})
+  // }
+
+  saveData(): void {
+    this.submitted = true;
+    if (this.loginForm.invalid) {
+      return;
     }
-    this.name = `${this.firstName} ${this.lastName}`
-    this.users.push({title: this.name, done: false})
+    const fullName = `${this.f.firstName.value} ${this.f.lastName.value}`
+    this.todos.push({title: fullName, done: false})
   }
 
 }
